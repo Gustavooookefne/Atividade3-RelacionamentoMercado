6cfc2d83fd717437c5f9730c53c9be4a6fe6d414
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 14:42:53 -0300

    feat: criando as duas class do model

---
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/model/Categoria.java
@@ -0,0 +1,20 @@
+package com.weg.RelacionamentoMercado.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_Categoria",nullable = false)
+    private String nomeCategoria;
+}
---
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/model/Produto.java
@@ -0,0 +1,28 @@
+package com.weg.RelacionamentoMercado.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome_produto", nullable = false)
+    private String nome;
+
+    @Column(name = "Preço_produto", nullable = false)
+    private double preco;
+
+    Categoria categoria;
+
+}
