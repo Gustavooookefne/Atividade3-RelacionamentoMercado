5c947aaf759d476cb9dcda385a8a06924809e2c6
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 14:19:55 -0300

    Add files via upload

---
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/RelacionamentoMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.weg.RelacionamentoMercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionamentoMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionamentoMercadoApplication.class, args);
+	}
+
+}
---
+++ RelacionamentoMercado/RelacionamentoMercado/src/test/java/com/weg/RelacionamentoMercado/RelacionamentoMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.RelacionamentoMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionamentoMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
