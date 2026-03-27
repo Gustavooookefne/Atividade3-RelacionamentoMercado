1cd72e30a1e8f273fd836ec5a7027d127f29e84e
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 14:45:34 -0300

    feat: Criado o CategoriaRepository

--- RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/repository/CategoriaRepository.java
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/repository/CategoriaRepository.java
@@ -1,4 +1,7 @@
 package com.weg.RelacionamentoMercado.repository;
 
-public class CategoriaRepository {
+import com.weg.RelacionamentoMercado.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria , Long> {
 }
