979f54f00f4a166d0e55b290fc7a47eb37b48206
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 16:01:56 -0300

    feat: Criado o CategoriaService

--- RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/service/CategoriaService.java
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/service/CategoriaService.java
@@ -1,4 +1,54 @@
 package com.weg.RelacionamentoMercado.service;
 
+import com.weg.RelacionamentoMercado.Dto.CategoriaDto.CategoriaRequestDto;
+import com.weg.RelacionamentoMercado.Dto.CategoriaDto.CategoriaResponseDto;
+import com.weg.RelacionamentoMercado.mapper.CategoriaMapper;
+import com.weg.RelacionamentoMercado.model.Categoria;
+import com.weg.RelacionamentoMercado.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class CategoriaService {
+
+    private final CategoriaRepository repository;
+    private final CategoriaMapper mapper;
+
+    public CategoriaResponseDto salvar (CategoriaRequestDto requestDto) {
+
+        Categoria categoria = new Categoria();
+
+        return mapper.paraDto(repository.save(categoria));
+    }
+
+    public List<CategoriaResponseDto> listarTodos () {
+        return repository.findAll()
+                .stream()
+                .map(mapper::paraDto)
+                .toList();
+    }
+
+    public CategoriaResponseDto listarPorId (Long id) {
+        Categoria categoria = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Lista não encontrada"));
+
+        return mapper.paraDto(categoria);
+    }
+
+    public CategoriaResponseDto atualizar (CategoriaRequestDto requestDto , Long id) {
+        Categoria categoria =  repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada"));
+
+        categoria.getNomeCategoria();
+
+        return mapper.paraDto(categoria);
+    }
+
+    public void deletar (Long id) {
+        repository.deleteById(id);
+    }
 }
