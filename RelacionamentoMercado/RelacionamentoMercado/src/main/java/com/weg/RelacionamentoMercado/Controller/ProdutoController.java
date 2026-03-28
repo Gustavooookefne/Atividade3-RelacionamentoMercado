526045aede39ae6e7a04b717430c5e7e0f2aca16
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 21:53:35 -0300

    feat: Adicionando ResponseEtity

--- RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/Controller/ProdutoController.java
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/Controller/ProdutoController.java
@@ -4,8 +4,11 @@ import com.weg.RelacionamentoMercado.Dto.ProdutoDto.ProdutoRequestDto;
 import com.weg.RelacionamentoMercado.Dto.ProdutoDto.ProdutoResponseDto;
 import com.weg.RelacionamentoMercado.service.ProdutoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,27 +20,35 @@ public class ProdutoController {
     private ProdutoService service;
 
     @PutMapping
-    public ProdutoResponseDto salvar (@RequestBody ProdutoRequestDto requestDto) {
-        return service.salvar(requestDto);
+    public ResponseEntity<ProdutoResponseDto> salvar (@RequestBody ProdutoRequestDto requestDto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.salvar(requestDto));
     }
 
     @GetMapping
-    public List<ProdutoResponseDto> listarTodos () {
-        return service.listarTodos();
+    public ResponseEntity<List<ProdutoResponseDto>> listarTodos () {
+            List<ProdutoResponseDto> responseDtos = new ArrayList<>();
+
+        return ResponseEntity.ok().body(responseDtos);
     }
 
     @GetMapping("/{id}")
-    public ProdutoResponseDto listarPorId (@PathVariable UUID id) {
-        return service.listarPorId(id);
+    public ResponseEntity<ProdutoResponseDto> listarPorId (@PathVariable UUID id) {
+            ProdutoResponseDto responseDto = service.listarPorId(id);
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @PostMapping("/{id}")
-    public ProdutoResponseDto atualizar (@RequestBody ProdutoRequestDto requestDto ,@PathVariable UUID id) {
-        return service.atualizar(requestDto , id);
+    public ResponseEntity<ProdutoResponseDto> atualizar (@RequestBody ProdutoRequestDto requestDto ,@PathVariable UUID id) {
+            ProdutoResponseDto responseDto = service.atualizar(requestDto , id);
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar (@PathVariable UUID id) {
+    public ResponseEntity<Void> deletar (@PathVariable UUID id) {
         service.deletar(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
