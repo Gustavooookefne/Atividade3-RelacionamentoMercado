4aa61a4fcd96105fc657fdd5c9efaeda7163e74f
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-27 21:41:06 -0300

    feat: Adicionando ResponseEtity

--- RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/Controller/CategoriaController.java
+++ RelacionamentoMercado/RelacionamentoMercado/src/main/java/com/weg/RelacionamentoMercado/Controller/CategoriaController.java
@@ -4,6 +4,8 @@ import com.weg.RelacionamentoMercado.Dto.CategoriaDto.CategoriaRequestDto;
 import com.weg.RelacionamentoMercado.Dto.CategoriaDto.CategoriaResponseDto;
 import com.weg.RelacionamentoMercado.service.CategoriaService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +18,35 @@ public class CategoriaController {
     private final CategoriaService service;
 
     @PutMapping
-    public CategoriaResponseDto salvar (@RequestBody CategoriaRequestDto requestDto) {
-        return service.salvar(requestDto);
+    public ResponseEntity<CategoriaResponseDto> salvar (@RequestBody CategoriaRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.salvar(requestDto));
     }
 
     @GetMapping
-    public List<CategoriaResponseDto> listarTodos () {
-        return service.listarTodos();
+    public ResponseEntity<List<CategoriaResponseDto>> listarTodos () {
+            List<CategoriaResponseDto> responseDtos = service.listarTodos();
+        return ResponseEntity.ok().body(responseDtos);
     }
 
     @GetMapping("/{id}")
-    public CategoriaResponseDto listarPorId (@PathVariable Long id) {
-        return service.listarPorId(id);
+    public ResponseEntity<CategoriaResponseDto> listarPorId (@PathVariable Long id) {
+            CategoriaResponseDto responseDto = service.listarPorId(id);
+
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @PostMapping("/{id}")
-    public CategoriaResponseDto atualizar (@RequestBody CategoriaRequestDto requestDto , @PathVariable Long id) {
-        return service.atualizar(requestDto , id);
+    public ResponseEntity<CategoriaResponseDto> atualizar (@RequestBody CategoriaRequestDto requestDto , @PathVariable Long id) {
+            CategoriaResponseDto responseDto = service.atualizar(requestDto , id);
+
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar (@PathVariable Long id) {
+    public ResponseEntity<Void> deletar (@PathVariable Long id) {
         service.deletar(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
